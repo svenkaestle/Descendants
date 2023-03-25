62e73375f25d2661ef2de557653ae37763a698eb
Sven <sven.kaestle@gmx.de>
2023-03-25 20:38:16 +0100

    chore: remove unused variables and clean up

--- app/src/main/java/dev/kaestle/descendants/DetailsFragment.kt
+++ app/src/main/java/dev/kaestle/descendants/DetailsFragment.kt
@@ -5,19 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.activityViewModels
 import dev.kaestle.descendants.databinding.FragmentDetailsBinding
+import dev.kaestle.descendants.viewmodel.SharedViewModel
 
 /**
- * A simple [Fragment] subclass as the details destination in the navigation.
+ * The [Fragment] to show a persons details.
  */
 class DetailsFragment : Fragment() {
 
     private var _binding: FragmentDetailsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private val sharedViewModel: SharedViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,9 +29,6 @@ class DetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.buttonDetails.setOnClickListener {
-            findNavController().navigate(R.id.action_DetailsFragment_to_ListFragment)
-        }
     }
 
     override fun onDestroyView() {
