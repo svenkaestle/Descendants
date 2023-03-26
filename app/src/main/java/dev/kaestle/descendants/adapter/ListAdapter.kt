57bfec6d54fd5e1f250f4f3fd0f15b89487a1663
Sven <sven.kaestle@gmx.de>
2023-03-26 05:22:32 +0200

    feat: add new default avatar and round the image view

--- app/src/main/java/dev/kaestle/descendants/adapter/ListAdapter.kt
+++ app/src/main/java/dev/kaestle/descendants/adapter/ListAdapter.kt
@@ -3,9 +3,9 @@ package dev.kaestle.descendants.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.imageview.ShapeableImageView
 import dev.kaestle.descendants.R
 import dev.kaestle.descendants.model.Person
 import java.time.format.DateTimeFormatter
@@ -25,14 +25,14 @@ class ListAdapter(private val persons: List<Person>) : RecyclerView.Adapter<List
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val person: Person = persons[position]
 
-        holder.ivAvatar.setImageResource(R.drawable.ic_launcher_background)
+        holder.ivAvatar.setImageResource(R.drawable.default_avatar)
         holder.tvName.text = person.name
         holder.tvBirthday.text = person.birthday.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
         holder.tvHeight.text = "${ person.height } cm"
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val ivAvatar: ImageView = itemView.findViewById(R.id.iv_avatar)
+        val ivAvatar: ShapeableImageView = itemView.findViewById(R.id.siv_avatar)
         val tvName: TextView = itemView.findViewById(R.id.tv_name)
         val tvBirthday: TextView = itemView.findViewById(R.id.tv_birthday)
         val tvHeight: TextView = itemView.findViewById(R.id.tv_height)
